cc788fd863e7b445630739e7645ed4a94d45654f
João Paulo Carvalho <jjpaulo2@protonmail.com>
2024-08-15 00:35:46 -0300

    feat - running multiple spiders with one command

---
+++ noticias_phb/__main__.py
@@ -0,0 +1,16 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from noticias_phb.spiders import modern_blogger, classic_blogger
+
+
+spiders = [
+    classic_blogger.ClassicBloggerSpider,
+    modern_blogger.ModernBloggerSpider,
+]
+
+if __name__ == '__main__':
+    settings = get_project_settings()
+    process = CrawlerProcess(settings)
+    for spider in spiders:
+        process.crawl(spider)
+    process.start()
